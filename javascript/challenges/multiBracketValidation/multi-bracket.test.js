6ad4ef59c64a2c872552d040241a9bbaa26e6904
Sayefdeen Alrawad <sayefdeen.alrawad@gmail.com>
2020-10-29 00:09:18 +0300

    fix CC 13 using stack

--- javascript/challenges/multiBracketValidation/multi-bracket.test.js
+++ javascript/challenges/multiBracketValidation/multi-bracket.test.js
@@ -6,17 +6,27 @@ const multiBracketValidation = require("./multi-bracket-validation");
 describe("Multi Bracket Validation ", () => {
   it("should return true if the sending string brackets was balanced ", () => {
     expect(multiBracketValidation("()[[Extra Characters]]")).toBeTruthy();
+    expect(multiBracketValidation("(){}[[]]")).toBeTruthy();
   });
   it("should return false if the sending string brackets was not balanced ", () => {
-    expect(multiBracketValidation("()[Extra Characters]]")).toBeFalsy();
+    expect(multiBracketValidation("{}(){}")).toBeTruthy();
   });
   it("should return false if the sending string was one bracket only", () => {
     expect(multiBracketValidation("(")).toBeFalsy();
-    expect(multiBracketValidation("}")).toBeFalsy();
+    expect(() => {
+      multiBracketValidation("}");
+    }).toThrow("Stack Is Empty");
     expect(multiBracketValidation("[")).toBeFalsy();
+    expect(multiBracketValidation("[}")).toBeFalsy();
   });
   it("should return true if the sending string brackets was balanced ", () => {
     expect(multiBracketValidation("{}{Code}[Fellows](())")).toBeTruthy();
+  });
+  it("should return false if the sending string brackets was not balanced", () => {
+    expect(multiBracketValidation("{(})")).toBeFalsy();
+  });
+  it("should return false if the sending string brackets was not balanced", () => {
+    expect(multiBracketValidation("[({}]")).toBeFalsy();
   });
   it("should return false if the sending string brackets was not balanced", () => {
     expect(multiBracketValidation("(](")).toBeFalsy();
