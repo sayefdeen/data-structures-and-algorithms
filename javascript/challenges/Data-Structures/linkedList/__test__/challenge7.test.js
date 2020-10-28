d068e1304acb9564dc4024fbf6e0fbd22b986be6
Sayefdeen Alrawad <sayefdeen.alrawad@gmail.com>
2020-10-28 22:29:19 +0300

    fix CC7 throwing exception

--- javascript/challenges/Data-Structures/linkedList/__test__/challenge7.test.js
+++ javascript/challenges/Data-Structures/linkedList/__test__/challenge7.test.js
@@ -17,8 +17,9 @@ describe("Challenge Number 7", () => {
     let testArray = [1, 2, 3, 4, 5];
     testArray.forEach((item) => llObject.append(item));
     let kValue = 6;
-    let index = llObject.kthFromEnd(kValue);
-    expect(index).toBeUndefined();
+    expect(() => {
+      llObject.kthFromEnd(kValue);
+    }).toThrow(`Index >  length of LinkedList`);
   });
 
   it("should return undefined if the value of the passed k < 0", () => {
@@ -26,8 +27,9 @@ describe("Challenge Number 7", () => {
     let testArray = [1, 2, 3, 4, 5];
     testArray.forEach((item) => llObject.append(item));
     let kValue = -1;
-    let index = llObject.kthFromEnd(kValue);
-    expect(index).toBeUndefined();
+    expect(() => {
+      llObject.kthFromEnd(kValue);
+    }).toThrow(`Can't pass a negative value`);
   });
 
   it("should return the index if k passed is 0 and linkedList length is equal to 1 ", () => {
