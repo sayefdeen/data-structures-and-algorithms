f44b94f73c5229ed158eea09791d7083a69faf03
Sayefdeen Alrawad <sayefdeen.alrawad@gmail.com>
2020-11-23 07:11:42 +0200

    Finsih Code Challenge 31

--- javascript/challenges/Data-Structures/hashTables/__test__/hash-table.test.js
+++ javascript/challenges/Data-Structures/hashTables/__test__/hash-table.test.js
@@ -1,5 +1,7 @@
+/* eslint-disable quotes */
 "use strict";
 const LinkedList = require("../../linkedList/lib/linked-list");
+const firstRepeatedWord = require("../../repeatedWords/repeated-word");
 const HashMap = require("../lib/hashtable");
 
 describe("Hash Table Tests", () => {
@@ -35,5 +37,24 @@ describe("Hash Table Tests", () => {
       let testLinkedList = hashMap.storage[hashKey];
       expect(testLinkedList).toBeInstanceOf(LinkedList);
     });
+  });
+
+  describe("Code Challenge 31 Repeated Word", () => {
+    it("should return the word (a) from this test case ", () => {
+      let testSent = "Once upon a time, there was a brave princess who...";
+      expect(firstRepeatedWord(testSent)).toBe("a");
+    });
+  });
+
+  it("should return the word (it) from this test case ", () => {
+    let testSent =
+      "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only...";
+    expect(firstRepeatedWord(testSent)).toBe("it");
+  });
+
+  it("should return the word (summer) from this test case ", () => {
+    let testSent =
+      "It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York...";
+    expect(firstRepeatedWord(testSent)).toBe("summer");
   });
 });
